1028e49b117dad253a0268e981d3f836a9f65d79
sk-azraf-sami <azraf7026official@gmail.com>
2025-03-07 15:55:20 +0600

    ✨ feat: configure PostgreSQL database connection

---
+++ backend/src/config/db.js
@@ -0,0 +1,28 @@
+const { Pool } = require('pg');
+require('dotenv').config({path: '../../.env'});
+
+/*
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_NAME:', process.env.DB_NAME);
+*/
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+// Test the database connection
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+  } else {
+    console.log('Database connection successful:', res.rows[0]);
+  }
+});
+
+module.exports = pool;
