81d1eb3aaa92f7f7ccd632cc6ac89a0fd6049531
sk-azraf-sami <azraf7026official@gmail.com>
2025-03-07 18:51:51 +0600

    ✨ feat: create user controller with register and login functions

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,25 @@
+const authService = require('../services/authService');
+
+const register = async (req, res) => {
+  try {
+    const newUser = await authService.registerUser(req.body);
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const { user, token } = await authService.loginUser(email, password);
+    res.status(200).json({ user, token });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  register,
+  login,
+};
