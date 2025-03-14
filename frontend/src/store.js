1d7ed614c75cb3bcfec3d0019f881ee1281fd397
sk-azraf-sami <azraf7026official@gmail.com>
2025-03-14 22:53:18 +0600

    ✨ feat: Configure Redux store

---
+++ frontend/src/store.js
@@ -0,0 +1,13 @@
+// filepath: /home/azraf-sami/Documents/hands-on-volunteering-platform/frontend/src/store.js
+import { configureStore } from '@reduxjs/toolkit';
+import { apiSlice } from './slices/apiSlice';
+
+const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+});
+
+export default store;
