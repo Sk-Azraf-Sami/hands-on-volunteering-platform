ef36d27a11e22e87c47b58de58b6d13f8a8803ea
sk-azraf-sami <azraf7026official@gmail.com>
2025-03-15 12:24:07 +0600

    ✨ feat: Add event slice for managing event state

---
+++ frontend/src/slices/eventSlice.js
@@ -0,0 +1,29 @@
+import { apiSlice } from './apiSlice';
+import { EVENTS_API } from '../constants';
+
+export const eventApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    createEvent: builder.mutation({
+      query: (data) => ({
+        url: `${EVENTS_API}/create`,
+        method: 'POST',
+        body: data,
+      }),
+    }),
+    getEvents: builder.query({
+      query: () => `${EVENTS_API}/list`,
+    }),
+    getEvent: builder.query({
+      query: (eventId) => `${EVENTS_API}/${eventId}`,
+    }),
+    joinEvent: builder.mutation({
+      query: (eventId) => ({
+        url: `${EVENTS_API}/join`,
+        method: 'POST',
+        body: { eventId },
+      }),
+    }),
+  }),
+});
+
+export const { useCreateEventMutation, useGetEventsQuery, useGetEventQuery, useJoinEventMutation } = eventApiSlice;
