4c0e9818dddff2a794bf7b43bcf51c04aeaa8ede
sk-azraf-sami <azraf7026official@gmail.com>
2025-03-15 14:04:09 +0600

    ✨ feat: Add Logout component

---
+++ frontend/src/components/Logout.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { logout } from '../slices/authSlice';
+
+const Logout = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate('/login');
+  };
+
+  return (
+    <button onClick={handleLogout} className="text-white">
+      Logout
+    </button>
+  );
+};
+
+export default Logout;
